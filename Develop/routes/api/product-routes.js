59f7402fe8cea6458fc2cbb8542ccd2e544d33a0
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-09-10 23:18:53 -0700

    Finished product-routes hopefully

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,12 +7,56 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+    {
+      model: Category,
+      attributes: ["id", "category_name"]
+    },
+    {
+      model: Tag,
+      attributes: ['tag_name'],
+      through: ProductTag
+    }
+  ],
+  })
+  .then((data) => {
+    res.status(200).json(data);
+  })
+  .catch((err) => {
+    res.status(400).json(err);
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id, {
+    include: [
+      {
+        model: Category,
+        attributes: ["id", "category_name"]
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name'],
+        through: ProductTag
+      }
+    ],
+  })
+  .then((data) => {
+    if(data){
+      res.status(200).json(data);
+    }
+    else{
+      res.status(404).json(" :( ");
+      return;
+    }
+  })
+  .catch((err) => {
+    res.status(400).json(err);
+  })
 });
 
 // create new product
@@ -91,6 +135,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((data) => {
+    res.status(200).json(data);
+  })
+  .catch((err) => {
+    res.status(400).json(err);
+  })
 });
 
 module.exports = router;
