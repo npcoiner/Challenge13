ea9c51e5cf6edc51a9f7e70b4dccad82f04e79b4
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-09-10 22:27:24 -0700

    Base model code

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category);
 // Categories have many Products
-
+Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
-
+Products.belongsToMany(Tags);
 // Tags belongToMany Products (through ProductTag)
+Tags.belongToMany(Tags);
 
 module.exports = {
   Product,
